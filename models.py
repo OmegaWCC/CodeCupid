c5848db54936b0f00b97837136d3b9c3885d9c85
Niklas Sheth <niklas@hypergolic.org>
2022-02-12 21:13:27 -0500

    create account system

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     name = db.Column(db.Text, unique=False, nullable=False)
     age = db.Column(db.Integer, nullable=False)
     interests = db.relationship('Interest', backref='user', lazy=True)
